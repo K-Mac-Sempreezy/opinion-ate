bd075a320047612fefe731b6ffa19e52c8324cca
Kristian MacDonald <kristian.macsempreezy@gmail.com>
2021-10-19 13:06:29 -0700

    set up cypress

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,19 @@
 module.exports = {
-  extends: ["react-app", "prettier"],
-  plugins: ["jest", "cypress", "prettier"],
-  parser: "babel-eslint",
+  extends: ['react-app', 'prettier'],
+  plugins: ['jest', 'cypress', 'prettier'],
+  parser: 'babel-eslint',
   env: {
     browser: true,
-    "cypress/globals": true,
+    'cypress/globals': true,
     es6: true,
-    "jest/globals": true,
+    'jest/globals': true,
   },
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
   rules: {
+    'prettier/prettier': 'warn',
   },
 };
